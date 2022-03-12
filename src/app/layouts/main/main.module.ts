4c1f9c64201783da9445df93b0dfb8a37a0908c4
Dave Salazar <salazardave93@gmail.com>
2022-03-11 23:50:42 -0500

    refactor layout components

--- src/app/layouts/main/main.module.ts
+++ src/app/layouts/main/main.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { FooterComponent } from 'src/app/modules/shared/components/footer/footer.component';
-import { HeaderComponent } from 'src/app/modules/shared/components/header/header.component';
-import { SidebarComponent } from 'src/app/modules/shared/components/sidebar/sidebar.component';
 import { MaterialModule } from 'src/app/modules/shared/material.module';
 import { SharedModule } from 'src/app/modules/shared/shared.module';
 import { MainComponent } from './main.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 
 
